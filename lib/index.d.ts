d4392f03b35eb11bc26f4430b68d197416360a4f
Rubén Beltrán del Río <subscriptions@unlimited.pizza>
2022-05-03 16:10:41 +0200

    Update lib/index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 type ValidationOrDefaultValue<T> = Schema<T> | T
 
-export default function getenv<T>(envName: string, joiValidation: ValidationOrDefaultValue, defaults?: T): T;
+export default function getenv<T>(envName: string, joiValidation: ValidationOrDefaultValue<T>, defaults?: T): T;
